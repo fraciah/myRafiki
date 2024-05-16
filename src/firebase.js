8646efd44cc5947db6b636e14096187abc7de5a3
fraciah <karfraciah@gmail.com>
2024-05-16 22:39:01 +0300

    feat: Add Firestore and Storage to Firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,6 +14,7 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
   };
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };  
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage();
