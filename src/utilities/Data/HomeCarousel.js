b52faaacd12500c8fa052bb2575f1c436c2d232e
fraciah <karfraciah@gmail.com>
2024-05-22 19:10:03 +0300

    Add utilities and resources

---
+++ src/utilities/Data/HomeCarousel.js
@@ -0,0 +1,27 @@
+import carousel1 from "../../assets/images/carousel1.jpg";
+import carousel2 from "../../assets/images/carousel2.png";
+import carousel3 from "../../assets/images/carousel3.png";
+import carousel4 from "../../assets/images/carousel4.png";
+
+export const homeCarousel = [
+    {
+        id: 1,
+        img: carousel1,
+        desc: "This is the first description. First description",
+    },
+    {
+        id: 2,
+        img: carousel2,
+        desc: "This is the second description. Second description",
+    },
+    {
+        id: 3,
+        img: carousel3,
+        desc: "This is the third description. Third description",
+    },
+    {
+        id: 4,
+        img: carousel4,
+        desc: "This is the fourth description. Fourth description",
+    },
+];
